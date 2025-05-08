a6bf4e6b21d2ceaa1efa96f4f187a2d09d997aec
Rohan-developer028 <rg3437370@gmail.com>
2025-05-08 10:48:33 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import "react-router-dom"
+import Login from './screen/Login';
+import Home from './screen/Home';
+import { BrowserRouter, Route ,Routes} from 'react-router-dom';
+import Banker from './screen/Banker';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <BrowserRouter>
+      <Routes>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/transacation' element={<Home/>} />
+      <Route path='/accounts' element={<Banker/>}/>
+      </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
